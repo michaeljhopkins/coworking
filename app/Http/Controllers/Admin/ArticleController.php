5ac8d1039359528d5219a699d802a0340a969948
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-04-07 16:00:38 +0300

    made code pretty

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -14,74 +14,39 @@ class ArticleController extends CrudController {
 						"entity_name_plural" => "articles",
 						"route" => "admin/article",
 
-
 						// *****
-						// Define the columns for the table view:
+						// COLUMNS
 						// *****
-						// "columns" => [
-						// 					[
-						// 						'name' => 'title',
-						// 						'title' => "The Title"
-						// 					],
-						// 					[
-						// 						'name' => 'slug',
-						// 						'title' => "The Slug"
-						// 					],
-						// 					[
-						// 						'name' => 'content',
-						// 						'title' => "The Content"
-						// 					],
-						// 			],
+						//
+						// Define the columns for the table view as an array:
+						//
+						"columns" => [
+											[
+												'name' => 'title',
+												'title' => "The Title"
+											],
+											[
+												'name' => 'slug',
+												'title' => "The Slug"
+											],
+											[
+												'name' => 'content',
+												'title' => "The Content"
+											],
+									],
+						//
 						// or as a string:
-						"columns" => "title,slug,content",
+						//
+						// "columns" => "title,slug,content",
 
 
 						// *****
-						// Define the fields for the "Add new item" view:
+						// CREATE FIELDS
 						// *****
-						// "create_fields" => [
-						// 						[
-						// 							'name' => 'title',
-						// 							'title' => 'Title',
-						// 							'type' => 'text',
-						// 							'placeholder' => 'Your title here'
-						// 						],
-						// 						[
-						// 							'name' => 'content',
-						// 							'title' => 'Content',
-						// 							'type' => 'textarea',
-						// 							'placeholder' => 'Your textarea text here'
-						// 						],
-						// 					],
-						// or as a string:
-						// "create_fields" => "title,content",
-
-
-						// *****
-						// Define the fields for the "Edit item" view:
-						// *****
-						// "update_fields" => [
-						// 						[
-						// 							'name' => 'title',
-						// 							'title' => 'Title',
-						// 							'type' => 'text',
-						// 							'placeholder' => 'Your title here'
-						// 						],
-						// 						[
-						// 							'name' => 'content',
-						// 							'title' => 'Content',
-						// 							'type' => 'textarea',
-						// 							'placeholder' => 'Your textarea text here'
-						// 						],
-						// 					],
-						// or as a string:
-						// "update_fields" => "title,content"
-
-
-						// *****
-						// ALTERNATIVE: Define both create_fields and update_fields in one variable:
-						// *****
-						"fields" => [
+						//
+						// Define the fields for the "Add new item" view as an array:
+						//
+						"create_fields" => [
 												[
 													'name' => 'title',
 													'title' => 'Title',
@@ -96,6 +61,56 @@ class ArticleController extends CrudController {
 												],
 											],
 						// or as a string:
+						// "create_fields" => "title,content",
+
+
+						// *****
+						// UPDATE FIELDS
+						// *****
+						//
+						// Define the fields for the "Edit item" view as an array:
+						//
+						"update_fields" => [
+												[
+													'name' => 'title',
+													'title' => 'Title',
+													'type' => 'text',
+													'placeholder' => 'Your title here'
+												],
+												[
+													'name' => 'content',
+													'title' => 'Content',
+													'type' => 'textarea',
+													'placeholder' => 'Your textarea text here'
+												],
+											],
+						// or as a string:
+						// "update_fields" => "title,content"
+
+
+						// *****
+						// FIELDS ALTERNATIVE
+						// *****
+						//
+						// Define both create_fields and update_fields in one array:
+						//
+						// "fields" => [
+						// 						[
+						// 							'name' => 'title',
+						// 							'title' => 'Title',
+						// 							'type' => 'text',
+						// 							'placeholder' => 'Your title here'
+						// 						],
+						// 						[
+						// 							'name' => 'content',
+						// 							'title' => 'Content',
+						// 							'type' => 'textarea',
+						// 							'placeholder' => 'Your textarea text here'
+						// 						],
+						// 					],
+						//
+						// or as a string:
+						//
 						// "fields" => "title,content",
 						);
 
