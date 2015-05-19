d90070ed3cc3bac6d9a14c73be543f18a74f6394
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-05-19 23:47:49 +0300

    minor dashboard modif

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,19 +13,6 @@
 @endsection
 
 @section('content')
-<!-- Default box -->
-  <div class="box">
-    <div class="box-header with-border">
-      <h3 class="box-title">Edit site content</h3>
-    </div>
-    <div class="box-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus id velit autem sunt itaque iure, quod saepe commodi quis maiores modi, sed similique provident, totam eveniet. Nemo accusantium temporibus, saepe.
-      <br>
-      <br>
-      For now, you can see a crud entity: <a href="{{ url('admin/article') }}" class="btn btn-primary">Articles</a>
-
-    </div><!-- /.box-body -->
-  </div><!-- /.box -->
 
   @if (Entrust::can('view-admin-panel'))
   <div class="box">
