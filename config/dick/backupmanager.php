07325bbbe8ac3665a9d2705df9d15cec2f0d4c51
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-06-26 21:30:14 +0300

    updated configuration file for backupmanager

--- config/dick/backupmanager.php
+++ config/dick/backupmanager.php
@@ -1,1 +1,18 @@
-<?php 
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Storage Disk
+	|--------------------------------------------------------------------------
+	|
+	| The disk where to store the .zip backups, as defined in config/filesystems.php
+	|
+	| Default: 'local', which has its root in storage_path()
+	|
+	*/
+
+	'disk' => 'local',
+
+];
