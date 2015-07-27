d6169f9e5f44d2738a5952cbbb9461a1999c441c
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-07-27 21:44:43 +0300

    Permissions and Roles models updated to use CrudTrait

--- app/Permission.php
+++ app/Permission.php
@@ -1,18 +1,11 @@
 <?php namespace App;
 
 use Zizaco\Entrust\EntrustPermission;
+use Dick\CRUD\CrudTrait;
 
 class Permission extends EntrustPermission
 {
-	/**
-	 * Since this class does not extend BaseModel, we need to define isColumnNullable() on this model, too.
-	 */
-	public static function isColumnNullable($column_name) {
-        $instance = new static; // create an instance of the model to be able to get the table name
-        $answer = DB::select( DB::raw("SELECT IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='".$instance->getTable()."' AND COLUMN_NAME='".$column_name."' AND table_schema='".env('DB_DATABASE')."'") )[0];
-
-        return ($answer->IS_NULLABLE == 'YES');
-    }
+    use CrudTrait;
 
     public function roles()
     {
--- app/Role.php
+++ app/Role.php
@@ -1,9 +1,12 @@
 <?php namespace App;
 
 use Zizaco\Entrust\EntrustRole;
+use Dick\CRUD\CrudTrait;
 
 class Role extends EntrustRole
 {
+    use CrudTrait;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -14,15 +17,5 @@ class Role extends EntrustRole
 	public function permissions()
     {
         return $this->belongsToMany(\Config::get('entrust.permission'), 'permission_role');
-    }
-
-	/**
-	 * Since this class does not extend BaseModel, we need to define isColumnNullable() on this model, too.
-	 */
-	public static function isColumnNullable($column_name) {
-        $instance = new static; // create an instance of the model to be able to get the table name
-        $answer = DB::select( DB::raw("SELECT IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='".$instance->getTable()."' AND COLUMN_NAME='".$column_name."' AND table_schema='".env('DB_DATABASE')."'") )[0];
-
-        return ($answer->IS_NULLABLE == 'YES');
     }
 }
