db2851818cf1b6e8646f9daf7767117ba40ac175
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-09-06 12:44:05 +0300

    categories id_parent column should be nullable in the migration

--- database/migrations/2015_08_04_130551_create_categories_table.php
+++ database/migrations/2015_08_04_130551_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
 
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('parent_id')->default(0);
+            $table->integer('parent_id')->default(0)->nullable();
             $table->integer('lft')->unsigned()->nullable();
             $table->integer('rgt')->unsigned()->nullable();
             $table->integer('depth')->unsigned()->nullable();
