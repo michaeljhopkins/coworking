800a121dafb5a0816e09696f597c92589ab3a312
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-09-06 12:31:17 +0300

    added pages migrations

---
+++ database/migrations/2015_09_06_091226_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('template');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('content');
+            $table->text('extras');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
